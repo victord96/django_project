ce2fea4530a9235d5fb2d78e1097122caeaba368
--replace-all <victor1996d@gmail.com>
2021-10-22 12:14:34 +0200

    Completed Migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -32,7 +32,7 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Model of BBDD for users in the sistem
     """
-    email = models.EmailField(max_lenght=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
