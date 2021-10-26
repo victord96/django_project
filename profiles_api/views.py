060a7932bed92f61e708140160ab34e646421329
--replace-all <victor1996d@gmail.com>
2021-10-26 18:57:16 +0200

    Created post api request

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """ Example of API View """
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return list of APIView features"""
@@ -15,3 +20,16 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a message with my name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )    
