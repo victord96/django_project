3e35996cbf522ce13dd11f65a2b6106c3fa41777
--replace-all <victor1996d@gmail.com>
2021-10-26 17:55:04 +0200

    Included apiview URL called hello-view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -14,4 +14,4 @@ class HelloApiView(APIView):
             'Is mapped to the URLs'
         ]
 
-        return Response({'message': 'Hello'}, an_apiview)
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
