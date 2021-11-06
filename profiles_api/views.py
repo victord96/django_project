8f4a31b792f3826f0afe20480ea65dd4dc77c860
--replace-all <victor1996d@gmail.com>
2021-11-06 20:44:51 +0100

    UserProfileViewSet : views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 
-from profiles_api import serializers
+from profiles_api import serializers, models
 
 
 class HelloApiView(APIView):
@@ -92,3 +92,8 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Destroy an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Create and update profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
